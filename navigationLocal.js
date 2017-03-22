b169697800038d388fa17416a1c091bf8f474788
John van Schalkwyk <johnvanschalkwyk@gmail.com>
2017-03-22 19:56:58 +0200

    Updated to make provisions for routes to be stored

---
+++ navigationLocal.js
@@ -0,0 +1,132 @@
+var MongoClient = require('mongodb').MongoClient;
+var assert = require('assert');
+var ObjectId = require('mongodb').ObjectID;
+var url = 'mongodb://localhost:27017/NavgationDatabase';
+
+
+//====================================================================================================
+
+var addNewRoute = function(db, callback) {
+    db.collection('routes').insertOne({
+        "routeID": "1",
+        "beginPoint": "Humanities",
+        "endPoint": "Law",
+        "waypoints": [{
+                "name": "Piazza",
+            },
+            {
+                "name": "TuksFm",
+            },
+            {
+                "name": "Centenary",
+            }
+        ]
+    }, function(err, result) {
+        assert.equal(err, null);
+        console.log("User preferences have been saved.");
+        callback();
+    });
+};
+//====================================================================================================
+
+var addUserPreferences = function(db, callback) {
+    db.collection('userpreferences').insertOne({
+        "userID": "14307317",
+        "preferences": [{
+                "type": "stairs",
+                "preferred": "false"
+            },
+            {
+                "type": "shortestroute",
+                "preferred": "true"
+            },
+            {
+                "type": "minimaltraffic",
+                "preferred": "true"
+            }
+        ]
+    }, function(err, result) {
+        assert.equal(err, null);
+        console.log("User preferences have been saved.");
+        callback();
+    });
+};
+
+//====================================================================================================
+
+var findUser = function(db, callback) {
+    var cursor = db.collection('userpreferences').find();
+    cursor.each(function(err, doc) {
+        assert.equal(err, null);
+        if (doc != null) {
+            console.dir(doc);
+        } else {
+            callback();
+        }
+    });
+};
+
+//====================================================================================================
+
+var findSpecificUser = function(db, callback) {
+    var cursor = db.collection('userpreferences').find({ "userID": "14307317" });
+    cursor.each(function(err, doc) {
+        assert.equal(err, null);
+        if (doc != null) {
+            console.dir(doc);
+        } else {
+            callback();
+        }
+    });
+};
+
+//====================================================================================================
+
+var displaySpecificRoute = function(db, callback) {
+    var cursor = db.collection('routes').find({ "routeID": "1" });
+    cursor.each(function(err, doc) {
+        assert.equal(err, null);
+        if (doc != null) {
+            console.dir(doc);
+        } else {
+            callback();
+        }
+    });
+};
+
+//====================================================================================================
+
+
+var updateUserPreference = function(db, callback) {
+    db.collection('userpreferences').updateOne({ "userID": "14307317" }, {
+        $set: { "preferences": "" }
+    }, function(err, results) {
+        console.log(results);
+        callback();
+    });
+};
+
+//====================================================================================================
+var removeUserData = function(db, callback) {
+    db.collection('userpreferences').deleteOne({ "userID": "14307317" },
+        function(err, results) {
+            console.log(results);
+            callback();
+        }
+    );
+};
+//====================================================================================================
+
+//====================================================================================================
+MongoClient.connect(url, function(err, db) {
+    assert.equal(null, err);
+    addUserPreferences(db, function() {}); // This will be used to insert a new User with preferences into the DataBase
+    findUser(db, function() {}); // This will return all the users in the UserPreferences collection
+    findSpecificUser(db, function() {}); // This will return a specific user and their preferences.
+    addNewRoute(db, function() {});
+    displaySpecificRoute(db, function() {});
+    //updateUserPreference(db, function() {});
+    removeUserData(db, function() {}); // Delete a user and all their data.
+
+    db.close();
+});
