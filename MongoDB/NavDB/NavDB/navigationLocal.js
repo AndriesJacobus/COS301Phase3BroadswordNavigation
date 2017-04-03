de7bbe08121ce82ae56d0da867b274372ed59920
JohnVanSchalkwyk <johnvanschalkwyk@gmail.com>
2017-04-03 23:33:25 +0200

    Console Return of Cached Nodes

--- MongoDB/NavDB/NavDB/navigationLocal.js
+++ MongoDB/NavDB/NavDB/navigationLocal.js
@@ -2,13 +2,15 @@ var MongoClient = require('mongodb').MongoClient;
 var assert = require('assert');
 var ObjectId = require('mongodb').ObjectID;
 var url = 'mongodb://localhost:27017/NavgationDatabase';
+var async = require('asyncawait/async');
+var await = require('asyncawait/await');
 
 // --------------------------------------------------------------------------------------
 //                 VARIABLES TO BE USED 
 
 var paths = [];
 var resultsLength;
-
+var cachedStatus = false;
 // --------------------------------------------------------------------------------------
 
 
@@ -24,28 +26,44 @@ var displaySpecificRoute = function(db, callback) {
 searchRouteInCachedRoutes("Humanities", "Law");
 
 function searchRouteInCachedRoutes(beginPoint, endPoint) {
-    console.log("searchRouteInCachedRoutes called.");
 
     //=================================================================================================
     MongoClient.connect(url, function(err, db) {
-        // displaySpecificRoute(db, function() {});
 
-        db.collection('routes').find({ "routeID": "1" }).toArray(function(err, results) {
-            // console.log(JSON.stringify(results));
-            resultsLength = results.length;
-            //console.log("The amount of results returned are : " + resultsLength);
-            var bp = results[0]['beginPoint'];
-            var ep = results[0]['endPoint'];
-            if (beginPoint == bp && endPoint == ep)
-                console.log("We have the route cached.");
-            else
-                console.log("The route was not cahced and needs to be calculated.");
+        db.collection('routes').find({ "beginPoint": beginPoint }).toArray(function(err, results) {
+
+            try {
+                //console.log(JSON.stringify(results));
+                resultsLength = results.length;
+                console.log("The amount of results returned are : " + resultsLength);
+
+                for (var i = 0; i < resultsLength; i++) {
+                    var bp = results[0]['beginPoint'];
+                    var ep = results[0]['endPoint'];
+
+                    if (beginPoint == bp && endPoint == ep) { // Traverse the results and see if any of them have the same start and endpoints as the requested ones.
+                        cachedStatus = true;
+                        paths.push(results[i]);
+                    }
+                }
+
+                if (cachedStatus == true) {
+                    console.log("We have the route cached."); // Indicate that we have a cached route available.
+                    for (var i = 0; i < paths.length; i++) {
+                        console.log(JSON.stringify(paths[i])); // Display the routes that are available based on the start and endpoint.
+                    }
+
+                } else
+                    console.log("The route was not cahced and needs to be calculated.");
+
+            } catch (error) {
+                console.log("No cached routs listed with specified starting point.");
+            }
         });
 
         db.close();
 
     });
     //=================================================================================================
-
 
 }
