e273b3da746ff4ad935ff9e27073a654bcf6ca86
AJ <u15226183@tuks.co.za>
2017-04-04 17:15:03 +0200

    Cleaned code and simplified

--- Nav Node Server/index.js
+++ Nav Node Server/index.js
@@ -11,6 +11,7 @@ app.post('/accept', function(req, res) {
     //console.log("JSON received: " + req.body);
     var start = req.body.start;
     var end = req.body.end;
+    
     console.log("\nNavigation Request received: ");
     console.log("-------------------------------");
     console.log("Start: " + start);
@@ -41,37 +42,37 @@ function calculateRoute(inStart, inEnd) {
     return json;
 }
 
-app.get('/getJsonFromNav', function(req, res) {
+//this function will send a get request to the gis with the json object containing end and start point
+function sendStartAndEndToGIS(start_,end_){
+    request.post('http://127.0.0.1:5000/getJsonFromNav', 
+        { json: { start: start_, end: end_ } }
+        function (error, response, body) {
+            if (!error && response.statusCode == 200) {
+                //console.log(body)
+                console.log("\Route received (from start to finish):\n");
+                console.log("Start: "  + response.body.start);
+                console.log("Middle: " + response.body.middle);
+                console.log("End: " + response.body.end);
+            }
+        }
+    );
+}
+
+//This is an example funciton of how GIS can receive the get request sent above
+app.post('/getJsonFromNav', function(req, res) {
     'use strict';
     var start = req.body.start;
     var end = req.body.end;   
-        if (res.statusCode == 200) {
-            //console.log(body)
-            console.log("\Route received (from start to finish):\n");
-            console.log("Start: "  + res.body.start);
-            //console.log("Middle: " + res.body.middle);
-            console.log("End: " + response.body.end);
-        }
     
+    console.log("\nRoute calculation Request received: ");
+    console.log("-------------------------------");
+    console.log("Start: " + start);
+    console.log("End: " + end);
+    console.log("Now calculating route...");
+
+    res.writeHead(200, { "Content-Type": "application/json" });
+    res.end(calculateRoute(start, end));        //replace this with GIS route calculating function that returns JSON object
 });
-
-function sendStartAndEndToGIS(start_,end_){
-//this function will send a getrequest to the gis with the json object containing end and start point
-
-request.post('http://127.0.0.1:5000/getJsonFromNav', 
-        { json: { start: start_, end: end_ } }
-         function (error, response, body) {
-        if (!error && response.statusCode == 200) {
-            //console.log(body)
-            console.log("\Route received (from start to finish):\n");
-            console.log("Start: "  + response.body.start);
-            console.log("Middle: " + response.body.middle);
-            console.log("End: " + response.body.end);
-        }
-    }
-);
-
-}
 
 var server = app.listen(port, function() {
     var host = server.address().address;
