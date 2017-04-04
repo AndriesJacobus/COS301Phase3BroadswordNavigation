23b747c899ed70e1adf3f8e5c16c36b884a467ce
yashvirbrijlal <yashvir.brijlal@gmail.com>
2017-04-04 16:13:17 +0200

    updated saveroute

--- MongoDB/saveRoute.js
+++ MongoDB/saveRoute.js
@@ -1,1 +1,10 @@
 // "C:\Program Files\MongoDB\Server\3.4\bin\mongod.exe"
+const mongoose = require('mongoose');
+
+mongoose.connect("mongodb://localhost/test");
+
+mongoose.connection.once('open', function(){
+	console.log("Connection successful.");
+}).on('error', function(){
+	console.log("Connnection failed.")
+});
